c5cc28fa0c11555a7d098adae1fe8f0b6b231c1b
nurgitlab <nurbek.math@gmail.com>
2022-08-18 19:28:31 +0600

    permutations solved

--- leetcode/permutations.js
+++ leetcode/permutations.js
@@ -1,23 +1,26 @@
 var permute = function (nums) {
   let ans = []
-  let mem = []
-  let n = nums.length
-  for (let i = 0; i < n; i++) {
-    for (let j = i; j < n; j++) {
-      if (nums[i] !== nums[j]) {
-        mem = nums
-        console.log(mem[i], ' <->', mem[j])
-        let m = mem[i]
-        mem[i] = mem[j]
-        mem[j] = m
-        console.log(mem)
-        ans = [...ans, mem]
-        mem = []
+  let allPers = function (arr, n) {
+    if (n < 2) {
+      let a = arr.slice()
+      ans.push(a)
+    } else {
+      for (let i = n - 1; i >= 0; i--) {
+        let mem = arr[i]
+        arr[i] = arr[n - 1]
+        arr[n - 1] = mem
+        allPers(arr, n - 1)
+        mem = arr[i]
+        arr[i] = arr[n - 1]
+        arr[n - 1] = mem
       }
     }
   }
 
+  allPers(nums, nums.length)
+
   return ans
 };
 
-console.log(permute([1, 2, 3]))
+
+console.log(permute([]))
