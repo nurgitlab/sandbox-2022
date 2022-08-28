c74b16cfa8f859030555b4ef2e87169aa17971c1
nurgitlab <nurbek.math@gmail.com>
2022-08-29 01:11:46 +0600

    solved: image-smoother

--- leetcode/archive/climbing-stairs.js
+++ leetcode/archive/climbing-stairs.js
@@ -1,20 +1,24 @@
 var climbStairs = function (n) {
-  if (n < 2) {
-    return 1
-  }
-
   let mem = []
-  for (let i = n; i > 0; i--) {
-    if (n === i) {
-      mem[i] = 1
-    } else if (i === n - 1) {
-      mem[i] = 1
+
+  for (let i = n - 1; i >= 0; i--) {
+    if (i === n - 1) {
+      mem.push(1)
+    } else if (i === n - 2) {
+      mem.push(2)
     } else {
-      mem[i] = mem[i + 1] + mem[i + 2]
+      let l = mem.length - 1
+
+      mem.push(mem[l] + mem[l - 1])
     }
   }
 
-  return (mem[1] + mem[2])
+  return mem[mem.length - 1]
 };
 
-console.log(climbStairs(2))
+console.log(climbStairs(3))
+
+// n = 4
+
+// 2 -> 4
+// 3 -> 4
