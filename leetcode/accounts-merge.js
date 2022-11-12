eb11170eaa1228fd3ff23ee55e2555649a6640f6
nurgitlab <nurbek.math@gmail.com>
2022-11-12 15:08:22 +0300

    solved: minimum-operations-to-make-array-equal

---
+++ leetcode/accounts-merge.js
@@ -0,0 +1,5 @@
+const accountsMerge = function(accounts) {
+  
+}
+
+console.log(accountsMerge([["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]))
