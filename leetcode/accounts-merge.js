dbbf6d77c25bf2bc6749c697604038bcffd9a6c0
nurgitlab <nurbek.math@gmail.com>
2022-11-12 15:50:57 +0300

    solved: max-increase-to-keep-city-skyline

--- leetcode/accounts-merge.js
+++
@@ -1,5 +0,0 @@
-const accountsMerge = function(accounts) {
-  
-}
-
-console.log(accountsMerge([["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]))
