c12723013ca20a1794cc8d98697151bf2909bec5
nurgitlab <nurbek.math@gmail.com>
2022-12-02 17:46:31 +0300

    solved: strong-password-checker-ii

---
+++ leetcode/strong-password-checker-ii.js
@@ -0,0 +1,99 @@
+const strongPasswordCheckerII = function(password) {
+  if (password.length < 8) {return false}
+  if (password.toLowerCase() === password) {
+    return false
+  }
+
+  if (password.toUpperCase() === password) {
+    return false
+  }
+
+  let numbers = 0
+  let special = 0
+
+  password.split('').forEach(el => {
+    if (el === '0') {
+      numbers++
+    }
+    if (el === '1') {
+      numbers++
+    }
+    if (el === '2') {
+      numbers++
+    }
+    if (el === '3') {
+      numbers++
+    }
+    if (el === '4') {
+      numbers++
+    }
+    if (el === '5') {
+      numbers++
+    }
+    if (el === '6') {
+      numbers++
+    }
+    if (el === '7') {
+      numbers++
+    }
+    if (el === '8') {
+      numbers++
+    }
+    if (el === '9') {
+      numbers++
+    }
+
+    if (el === '!') {
+      special++
+    }
+    if (el === '@') {
+      special++
+    }
+    if (el === '#') {
+      special++
+    }
+    if (el === '$') {
+      special++
+    }
+    if (el === '%') {
+      special++
+    }
+    if (el === '^') {
+      special++
+    }
+
+    if (el === '&') {
+      special++
+    }
+    if (el === '*') {
+      special++
+    }
+    if (el === '(') {
+      special++
+    }
+    if (el === ')') {
+      special++
+    }
+    if (el === '+') {
+      special++
+    }
+    if (el === '-') {
+      special++
+    }
+  })
+
+  if (numbers === 0) {
+    return false
+  }
+
+  if (special === 0) {
+    return false
+  }
+
+  for (let i = 0; i < password.length - 1; i++) {
+    if (password[i] === password[i+1]) {return false}
+  }
+  return true
+}
+
+console.log(strongPasswordCheckerII("IloveLe3tcode!"))
