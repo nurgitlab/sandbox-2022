6442c9277eaf2695dab15b07e012a7a81dad01a7
nurgitlab <nurbek.math@gmail.com>
2022-12-05 13:33:08 +0300

    solved: longest-subsequence-with-limited-su

--- leetcode/minimum-time-to-type-word-using-special-typewriter.js
+++ leetcode/minimum-time-to-type-word-using-special-typewriter.js
@@ -1,16 +1,16 @@
-const minTimeToType = function(word) {
+const minTimeToType = function (word) {
   let current = 'a'
   let ans = 0
   word.split('').forEach(el => {
-    ans+=distation(el, current)
-    ans+=1
+    ans += distation(el, current)
+    ans += 1
     current = el
   })
 
   return ans
 }
 
-function distation (l1, l2) {
+function distation(l1, l2) {
   l1 = l1.charCodeAt(0) - 97
   l2 = l2.charCodeAt(0) - 97
   return Math.min(
