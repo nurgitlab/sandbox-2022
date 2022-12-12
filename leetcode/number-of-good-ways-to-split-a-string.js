9f4e8657b5e38aaa05d00998a304728a704b441e
nurgitlab <nurbek.math@gmail.com>
2022-12-12 18:39:32 +0300

    solved: reformat-phone-number

--- leetcode/number-of-good-ways-to-split-a-string.js
+++ leetcode/number-of-good-ways-to-split-a-string.js
@@ -6,6 +6,7 @@ const numSplits = function (s) {
   let mem2 = {}
   const lus = new Set(s.split(''))
   let c2 = lus.size
+
   s.split('').forEach(el => {
     if (mem2[el] === undefined) {
       mem2[el] = 0
@@ -14,11 +15,19 @@ const numSplits = function (s) {
   })
 
   for (let i = 0; i < s.length; i++) {
-    if (mem1[s[i]] === undefined) {mem1[s[i]] = true; c1++}
+    if (mem1[s[i]] === undefined) {
+      mem1[s[i]] = true;
+      c1++
+    }
     mem2[s[i]]--
-    if (mem2[s[i]] === 0) {delete mem2[s[i]]; c2--}
+    if (mem2[s[i]] === 0) {
+      delete mem2[s[i]];
+      c2--
+    }
 
-    if (c1 === c2) {ans++}
+    if (c1 === c2) {
+      ans++
+    }
   }
 
   return ans
