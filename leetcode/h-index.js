f6c4365b2e5b5de86f0517ccf5ecbd7240a9b59e
nurgitlab <nurbek.math@gmail.com>
2023-01-01 13:33:17 +0300

    solved: h-index

---
+++ leetcode/h-index.js
@@ -0,0 +1,14 @@
+const hIndex = function(citations) {
+  citations = citations.sort((a,b) => b -a)
+  let ans = 0
+
+  console.log(citations)
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] >= i + 1) {ans = i + 1}
+  }
+
+
+  return ans
+}
+
+console.log(hIndex([1,3,1]))
