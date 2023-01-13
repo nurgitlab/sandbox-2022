050d71c41210a55129c5bd3a688e33a6475dc70a
nurgitlab <nurbek.math@gmail.com>
2023-01-13 12:25:54 +0300

    solved: pascals-triangle-ii

--- leetcode/basic-calculator.js
+++ leetcode/basic-calculator.js
@@ -1,38 +1,102 @@
 const calculate = function (s) {
-  return solveWithout(s)
+  s = deleteSpaces(s)
+  while (true) {
+    let sk = 0
+    let sm = 0
+    let smId = -1
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === '(') {
+        sk++
+      }
+      if (s[i]===')') {
+        sk--
+      }
+
+      if (sm < sk) {
+        sm = sk
+        smId = i
+      }
+    }
+
+    if (sm > 0) {
+      let r = ''
+
+      let rc = 0
+      for (let i = smId; i < s.length; i++) {
+        rc++
+        if (s[i] === ')') {break}
+        if (s[i] === '(') {continue}
+        r+=s[i]
+      }
+      s = s.split('')
+      for (let i = smId; i < smId + rc; i++) {
+        s[i] = ''
+      }
+      s[smId] = String(sumWithout(r))
+      s = s.join('')
+      s = res(s)
+    }
+    if (sm === 0) {break}
+  }
+
+
+  return sumWithout(s)
 }
 
-function solveWithout(str) {
-  let deletedSpaces = ''
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] !== ' ') {
-      deletedSpaces += str[i]
+function deleteSpaces(str) {
+  let ans = ''
+  str.split('').forEach(el => {
+    if (el !== ' ') {
+      ans += el
     }
-  }
-
-  let nums = []
-  let local = ''
-  let operations = []
-  let ans = 0
-  for (let i = 0; i < deletedSpaces.length; i++) {
-    if (deletedSpaces[i] === '+'
-      || deletedSpaces[i] === '-') {
-      operations.push(deletedSpaces[i])
-      nums.push(Number(local))
-      local = ''
-    } else {
-      local+=deletedSpaces[i]
-    }
-  }
-
-  if (local.length > 0) {nums.push(Number(local))}
-
-  let m = 0
-  nums.forEach(n => {
-    if (operations[m] === '+') {}
-    m++
   })
-  return nums
+
+  return ans
 }
 
-console.log(calculate('2-101  + 2 '))
+function sumWithout(str) {
+  let nums = []
+  let n = ''
+  for (let i = 0; i < str.length - 1; i++) {
+    n += str[i]
+    if (str[i + 1] === '+' || str[i + 1] === '-') {
+      nums.push(n)
+      n = ''
+    }
+  }
+
+  n += str[str.length - 1]
+  nums.push(n)
+  nums = nums.map(el => Number(el))
+  let sum = 0
+  nums.forEach(el => sum += el)
+
+  return sum
+}
+
+function res (str) {
+  str = str.split('')
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === '+' && str[i+1] === '+') {
+      str[i]='+'
+      str[i+1]=''
+    }
+    if (str[i] === '+' && str[i+1] === '-') {
+      str[i]='-'
+      str[i+1]=''
+    }
+    if (str[i] === '-' && str[i+1] === '+') {
+      str[i]='-'
+      str[i+1]=''
+    }
+    if (str[i] === '-' && str[i+1] === '-') {
+      str[i]='+'
+      str[i+1]=''
+    }
+  }
+
+  return str.join('')
+}
+
+
+console.log(calculate("(5-(1+(5)))"))
