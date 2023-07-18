25b0ac3a887339164e150e9d0867848579988035
nurgitlab <nurbek.math@gmail.com>
2023-07-19 02:08:41 +0300

    added: project inited

---
+++ coffee-layout/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.module.scss'
+
+export const Header = () => {
+    return (
+        <div
+            className={styles.container}
+        >
+            11
+        </div>
+    )
+}
